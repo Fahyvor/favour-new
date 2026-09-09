3cb8ef7a40da7f5f66c5f8936e1b4645a1000756
Fahyvor <favourokafor30@gmail.com>
2026-09-09 15:43:16 +0100

    match nexx

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -16,7 +16,7 @@ const Portfolio = () => {
       id: 17,
       image: portfolio18,
       githubUrl: "https://github.com/Fahyvor/match-nexx",
-      siteUrl: "https://match-nexx.click",
+      siteUrl: "https://match-nexx.com",
       animation: 'flip-right'
     },
     {
